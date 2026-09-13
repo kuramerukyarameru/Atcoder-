2a896f8c48da58cef4edf4b188b3ebee60c07b2d
kuramerukyarameru <nagisa.miyazaki1116@gmail.com>
2026-09-13 16:20:22 +0900

    Create tempre.cpp

---
+++ tempre.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<numeric>
+#include<iomanip>
+#include<map>
+#include<stack>
+#include<queue>
+#include<tuple>
+#include<utility>
+#include<deque>
+#include<functional>
+#include<random>
+#include<chrono>
+#include<cstdint>
+#include<cstdlib>
+using namespace std;
+using ll = long long;
+#define el '\n'
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,l,n) for(int i=l;i<n;i++)
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+template<class T> using pq = priority_queue<T>;
+template<class T> using pq_ = priority_queue<T,vector<T>,greater<T>>;
+template<class T>inline bool chmin(T&a,T b){if(a<=b){return false;}else{a=b;return true;}};
+template<class T>inline bool chmax(T&a,T b){if(a>=b){return false;}else{a=b;return true;}};
+const int dr4[4]={0,-1,0,1};const int dc4[4]={-1,0,1,0};
+const int dr8[8]={-1,-1,-1,0,1,1,1,0};const int dc8[8]={-1,0,1,1,1,0,-1,-1};
+//左(左上)から時計回り
+
+//AHC用
+//auto start=chrono::steady_clock::now();//今この瞬間を保存
+//auto now = chrono::steady_clock::now();
+//double t = chrono::duration<double>(now-start).count();//数値を取り出す。
+//
+
+
+int main(){
+    ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	
+	return 0;
+}
+	
